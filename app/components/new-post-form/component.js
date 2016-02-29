0cfb68e60e3524c8b77028a5609ecb2774091e57
Gregory K and Patrick Sullivan <gregkanteres@gmail.com>
2016-02-29 12:00:16 -0800

    add dynamic post creation form

---
+++ app/components/new-post-form/component.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  showCategories: false,
+  showPostForm: false,
+  currentOverlord: null,
+  currentCategory: null,
+
+  actions: {
+    showCategories(overlord) {
+      this.set('showCategories', true);
+      this.set('currentOverlord', overlord);
+    },
+
+    hideCategories() {
+      this.set('showCategories', false);
+    },
+
+    showPostForm(category) {
+      this.set('showPostForm', true);
+      this.set('currentCategory', category);
+    }
+  }
+});
--- app/new-post/route.js
+++ app/new-post/route.js
@@ -2,11 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  model() {
+    return this.store.findAll('categoryOverlord');
+  },
+
   actions: {
     createPost(params) {
       var newPost = this.store.createRecord("post", params);
       newPost.save();
       this.transitionTo('index');
-    }
+    },
   }
 });
